8e089ea6b17859257db807e09bf6420964f0384d
Devops 911 <clickmamaheti@gmail.com>
2026-07-19 23:18:59 +0700

    Add lib/banner.py

---
+++ lib/banner.py
@@ -0,0 +1,11 @@
+from lib.args import args
+
+banner = """\033[96m
+╔══════════════════════════════════════╗
+║       \033[95mPHONIA-ID v1.0\033[96m              ║
+║    \033[93mOSINT Nomor HP Indonesia\033[96m       ║
+║  \033[92mPowered by DevCult XII\033[96m           ║
+╚══════════════════════════════════════╝\033[0m
+"""
+
+__version__ = "1.0"
